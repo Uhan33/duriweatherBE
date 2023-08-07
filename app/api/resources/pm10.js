be0af734841a7ba6321f8430d1cb6d096cd2e433
Uhan33 <jazzcafe12@kangwon.ac.kr>
2023-08-07 16:58:39 +0900

    미세먼지 api 추가(pm10.js)
    미세먼지 농도와 등급을 알려주는 api
    /api/pm10/value , /api/pm10/grade

---
+++ app/api/resources/pm10.js
@@ -0,0 +1,39 @@
+// 대기오염농도 API
+const express = require('express');
+const router = express.Router();
+
+var url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty';
+var queryParams = '?' + encodeURIComponent('serviceKey') + '=ySgwAA4cxcbx7n9F5i08pYGiFyS92Wc%2BjEdGYYvRsa4xCZlP0IbjUpelNxOdowDfjVmlKawIKfmnCnTAKmBLgQ%3D%3D'; /* Service Key*/
+queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+queryParams += '&' + encodeURIComponent('stationName') + '=' + encodeURIComponent('중앙로'); /* 강원도 춘천시 중앙로 관측소*/
+queryParams += '&' + encodeURIComponent('dataTerm') + '=' + encodeURIComponent('DAILY'); /* 1일 대기오염 정보 */
+queryParams += '&' + encodeURIComponent('ver') + '=' + encodeURIComponent('1.3'); /* */
+
+url = url + queryParams;
+    
+router.get("/", async (req, res) => {       //대기 오염 시간대별 총 목록
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.send(data);
+});
+
+router.get("/value", async (req, res) => {      //현재 시간 대기오염 지수
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.send(data.response.body.items[0].pm10Value);
+});
+
+/* 미세먼지 등급 1 : 좋음, 2 : 보통, 3 : 나쁨, 4 : 매우나쁨 
+*/
+router.get("/grade", async (req, res) => {      //현재 시간 대기오염 등급
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.send(data.response.body.items[0].pm10Grade);
+});
+
+module.exports = router;
--- app/api/router.js
+++ app/api/router.js
@@ -4,6 +4,7 @@ const apiRouter = express.Router();
 apiRouter.use("/hello", require("./resources/hello"));
 apiRouter.use("/vilageFcst", require("./resources/vilageFcst"));
 apiRouter.use("/ultraSrtNcst", require("./resources/ultraSrtNcst"));
+apiRouter.use("/pm10", require("./resources/pm10"));
 
 apiRouter.use("/user", require("./resources/users"));
 
