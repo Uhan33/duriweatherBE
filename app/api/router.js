c1325a5ef20279a31b7db362c663585aebf96d44
Uhan33 <jazzcafe12@kangwon.ac.kr>
2023-08-08 20:13:00 +0900

    간단 API 목록 가이드 구현
    /api

--- app/api/router.js
+++ app/api/router.js
@@ -7,5 +7,6 @@ apiRouter.use("/pm10", require("./resources/pm10"));
 apiRouter.use("/comment", require("./resources/comment"));
 
 apiRouter.use("/user", require("./resources/users"));
+apiRouter.use("/", require("./guide"));
 
 module.exports = apiRouter;
