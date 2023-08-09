4a049441b208868ca098a8ee80b63c11b7c7865c
Uhan33 <jazzcafe12@kangwon.ac.kr>
2023-08-09 16:30:45 +0900

    날씨 코드와 강수 코드 item 인덱스가 맞지 않아 수정

--- app/api/resources/vilageFcst.js
+++ app/api/resources/vilageFcst.js
@@ -56,7 +56,7 @@ router.get("/sky", async (req, res) => {    // 맑음 : 1, 구름조금 : 2, 구
         if(hour == data.response.body.items.item[i].fcstTime)
             break;
     }
-    res.send(data.response.body.items.item[i+6].fcstValue);
+    res.send(data.response.body.items.item[i+5].fcstValue);
 });
 
 // 강수형태를 코드로 보내줌
@@ -69,7 +69,7 @@ router.get("/pyt", async (req, res) => {    // 없음 : 0, 비 : 1, 비/눈 : 2,
         if(hour == data.response.body.items.item[i].fcstTime)
             break;
     }
-    res.send(data.response.body.items.item[i+7].fcstValue);
+    res.send(data.response.body.items.item[i+6].fcstValue);
 });
 
 module.exports = router;
