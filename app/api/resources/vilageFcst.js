f414efc5f35c028d60eb921fe2ce41699659cd86
Uhan33 <jazzcafe12@kangwon.ac.kr>
2023-08-16 15:40:15 +0900

    TMN 수정, 그 외 pop, reh, 등등 시간 수정

--- app/api/resources/vilageFcst.js
+++ app/api/resources/vilageFcst.js
@@ -110,7 +110,7 @@ router.get("/ptycheck", async (req, res) => {    // 없음 : 0, 비 : 1, 비/눈
     const data = await response.json();
 
     for(var i = 0; i < 1000; i++) {
-        if(hourCheck == data.response.body.items.item[i].fcstTime && now == data.response.body.items.item[i].fcstDate && "PTY" == data.response.body.items.item[i].category)
+        if(hour == data.response.body.items.item[i].fcstTime && now == data.response.body.items.item[i].fcstDate && "PTY" == data.response.body.items.item[i].category)
             break;
     }
     res.send(data.response.body.items.item[i]);
@@ -122,7 +122,7 @@ router.get("/reh", async (req, res) => {    // %로 내보내는듯.
     const data = await response.json();
 
     for(var i = 0; i < 1000; i++) {
-        if(hourCheck == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "REH" == data.response.body.items.item[i].category)
+        if(hour == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "REH" == data.response.body.items.item[i].category)
             break;
     }
     res.send(data.response.body.items.item[i].fcstValue);
@@ -134,7 +134,7 @@ router.get("/wsd", async (req, res) => {
     const data = await response.json();
 
     for(var i = 0; i < 1000; i++) {
-        if(hourCheck == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "WSD" == data.response.body.items.item[i].category)
+        if(hour == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "WSD" == data.response.body.items.item[i].category)
             break;
     }
     res.send(data.response.body.items.item[i].fcstValue);
@@ -146,7 +146,7 @@ router.get("/vec", async (req, res) => {
     const data = await response.json();
 
     for(var i = 0; i < 1000; i++) {
-        if(hourCheck == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "VEC" == data.response.body.items.item[i].category)
+        if(hour == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "VEC" == data.response.body.items.item[i].category)
             break;
     }
     res.send(data.response.body.items.item[i].fcstValue);
@@ -157,7 +157,7 @@ router.get("/pop", async (req, res) => {
     const data = await response.json();
 
     for(var i = 0; i < 1000; i++) {
-        if(hourCheck == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "POP" == data.response.body.items.item[i].category)
+        if(hour == data.response.body.items.item[i].fcstTime && fNow == data.response.body.items.item[i].fcstDate && "POP" == data.response.body.items.item[i].category)
             break;
     }
     res.send(data.response.body.items.item[i].fcstValue);
@@ -179,7 +179,7 @@ router.get("/tmn", async (req, res) => {
     const data = await response.json();
 
     for(var i = 0; i < 1000; i++) {
-        if(fNow == data.response.body.items.item[i].fcstDate && "TMN" == data.response.body.items.item[i].category)
+        if(fNow == data.response.body.items.item[i].fcstDate && "TMP" == data.response.body.items.item[i].category && "0600" == data.response.body.items.item[i].fcstTime)
             break;
     }
     res.send(data.response.body.items.item[i].fcstValue);
